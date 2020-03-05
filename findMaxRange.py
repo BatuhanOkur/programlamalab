54fe62f6d214b2e074d5c9c52181df4124cedc7e
Batuhan Okur <44201180+BatuhanOkur@users.noreply.github.com>
2020-03-05 12:42:34 +0200

    findMaxRange.py eklendi.

---
+++ findMaxRange.py
@@ -0,0 +1,23 @@
+def findMaxRange_i(inlist=[4, -3, 5, -2, -1, 2, 6, -2]):
+    max = 0
+    for i in range(len(inlist)):
+        for j in range(i+1, len(inlist)):
+            # print(i, j)
+            t = 0
+            for k in range(i,j+1):
+                t = t + inlist[k]
+            if t > max:
+                max = t
+                i_1, i_2 = i, j
+    return max , i_1, i_2
+
+def findMaxRange_ii(inlist=[4, -3, 5, -2, -1, 2, 6, -2]):  
+    max = 0
+    for i in range(len(inlist)):
+        t = 0
+        for j in range(i, len(inlist)):
+            t = t + inlist[j]
+            if t > max:
+                max = t
+                i_1, i_2 = i, j
+    return max , i_1, i_2
