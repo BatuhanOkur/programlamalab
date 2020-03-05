e66b27866acbaac0189d77ff055ec4a15b1acacd
Batuhan Okur <44201180+BatuhanOkur@users.noreply.github.com>
2020-03-05 12:10:31 +0200

    itemcounter.py eklendi.

---
+++ itemcounter.py
@@ -0,0 +1,9 @@
+def my_h(liste):
+  my_d={}
+  for item in liste:
+    if item not in my_d:
+      my_d[item]=1
+    else:
+      my_d[item]=item+1
+  return my_d
+print(my_h([2,3,4,6,2,5,6,6,6,6,6,6,6,2]))
