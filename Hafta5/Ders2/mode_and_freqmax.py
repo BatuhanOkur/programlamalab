3ee74be96b704093accda9776c2734bca27188f1
Batuhan Okur <44201180+BatuhanOkur@users.noreply.github.com>
2020-04-03 14:06:56 +0300

    Create mode_and_freqmax.py

---
+++ Hafta5/Ders2/mode_and_freqmax.py
@@ -0,0 +1,29 @@
+# my_frequency_with_dict ve my_frequency_with_list_of_tuple "Ders1" dosyasında mevcuttur burada tanımlanmış kabul edilmiştir.
+
+my_list=get_n_random_numbers(10)
+my_freq_dict=my_frequency_with_dict(my_list)
+my_freq_list=my_frequency_with_list_of_tuples(my_list)
+
+def my_mode_in_dict(my_hist_d): #Bir kümedeki modu ve kaç kere tekrar ettiğini döndürür.
+    freq_max=-1
+    mode=-1
+    for key in my_freq_dict:
+        print(key,my_hist_d[key])
+        if my_hist_d[key]>freq_max:
+            freq_max=my_hist_d[key]
+            mode=key
+    return mode,freq_max
+
+def my_mode_in_list(my_hist_l): #Bir listedeki modu ve kaç kere tekrar ettiğini döndürür.
+    freq_max=-1
+    mode=-1
+    for item,frequency in my_hist_l:
+        print(item,frequency)
+        if frequency>freq_max:
+            freq_max=frequency
+            mode=item
+    return mode,freq_max
+
+result_1=my_mode_in_dict(my_freq_dict)
+result_2=my_mode_in_list(my_freq_list)
+print(result_1,result_2)
